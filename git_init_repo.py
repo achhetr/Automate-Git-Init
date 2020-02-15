f3bde81699c49da1f082c91311b2bddc720dbc1a
Akash Chhetri <akash.chhetri1@gmail.com>
2020-02-16 10:53:23 +1100

    04- Fixed indentation bug

--- git_init_repo.py
+++ git_init_repo.py
@@ -1,15 +1,20 @@
 import os
 
 def git_command(git_path):
+    # git command for terminal
     git_terminal_command = ['echo "# Automate-Git-Init" >> README.md', 'git init',
                             'git add README.md', 'git add .','git commit -m "first commit"',
                             'git remote add origin %s' % git_path,
                             'git push -u origin master']
     
+    # iterate every command
     for comd in git_terminal_command:
         os.system(comd)
-    print("SUCCESS")
 
-# main
+    print("\n\nSUCCESS")
+
+##
+## main program
+##
 g_path = input("Copy git repo path here and press enter:  ")
 git_command(g_path)
--- git_init_repo_hub.py
+++ git_init_repo_hub.py
@@ -1,13 +1,18 @@
 import os
 
 def git_command():
-        git_terminal_command = ['echo "# Automate-Git-Init" >> README.md', 'git init',
+    # git command for terminal
+    git_terminal_command = ['echo "# Automate-Git-Init" >> README.md', 'git init',
                             'git add README.md', 'git add .','git commit -m "first commit"',
                             'hub create', 'git push -u origin master']
     
+    # iterate every command
     for comd in git_terminal_command:
         os.system(comd)
-    print("SUCCESS")           
+    
+    print("\n\nSUCCESS")          
 
-# main
+##
+## main program
+##
 git_command()
